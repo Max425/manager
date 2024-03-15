5c9b470c94c195e8a46969a8b365b94d8447a5bf
Maxim Sikanov <max.4251474@yandex.ru>
2024-03-15 21:00:38 +0300

    #10 add employee fields

--- backend/migrations/000001_init.down.sql
+++ backend/migrations/000001_init.down.sql
@@ -2,3 +2,6 @@ drop table if exists company cascade;
 drop table if exists employee cascade;
 drop table if exists project cascade;
 drop table if exists employee_project cascade;
+
+drop function if exists decrease_active_project_count_trigger;
+drop function if exists increase_project_count_trigger;
